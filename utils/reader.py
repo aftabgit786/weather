c8e31b29927f374d152434e70ab097af67715e0e
aftabgit786 <gb485585@gmail.com>
2023-03-16 16:56:21 +0500

    file add

--- utils/reader.py
+++ utils/reader.py
@@ -1,1 +1,19 @@
-import csv
+def get_content_of_files(file_content):
+    file_values = []
+    for row in file_content.split("\n")[1:]:
+        file_values.append(row.split(","))
+        return file_values
+
+
+def read_file():
+    file_values = []
+    with open("/home/aftab/Desktop/Data/ISDP/weather/files/f1.csv") as file_reader:
+        file_content = file_reader.read()
+        file_values += get_content_of_files(file_content)
+        file_reader.close()
+
+        return file_values
+
+file_values = read_file()
+for file_value in file_values:
+    print(file_value)
