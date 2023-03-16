07cae22d662f0c5dc028a1f44b350fce3edb9b48
aftabgit786 <gb485585@gmail.com>
2023-03-16 18:55:32 +0500

    change variabel name

--- tasks/t1.py
+++ tasks/t1.py
@@ -6,7 +6,7 @@ for file_value in file_values:
     date = file_value[0]
     max_temp = float(file_value[1])
     min_temp = float(file_value[3])
-    average_of_max_with_min = (max_temp - min_temp) /2
+    diffrent_max_with_min = max_temp - min_temp
 
-    print(f"{date}, Maximum-Temp is {max_temp} AND Minimum-Temp is {min_temp} of Average is: {average_of_max_with_min}")
+    print(f"{date}, Maximum-Temp is {max_temp} AND Minimum-Temp is {min_temp} Diffrance, {diffrent_max_with_min}")
 
