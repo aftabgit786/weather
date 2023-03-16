3fc74282e4b23d85665a4d1523283fa822a529ae
aftabgit786 <gb485585@gmail.com>
2023-03-16 17:40:05 +0500

    complete task-1

---
+++ tasks/t1.py
@@ -0,0 +1,10 @@
+from weather.utils.reader import read_file
+file_values = read_file()
+
+for file_value in file_values:
+    date = file_value[0]
+    max_temp = float(file_value[1])
+    min_temp = float(file_value[3])
+    diffrent_max_with_min = max_temp - min_temp
+
+    print(date, "Maximum-Temp AND Minimum Temp-Diffrance", diffrent_max_with_min)
