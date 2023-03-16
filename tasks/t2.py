c896b45db5140e2c71957aa55764369b1a106b06
aftabgit786 <gb485585@gmail.com>
2023-03-16 18:33:17 +0500

    t2 add

---
+++ tasks/t2.py
@@ -0,0 +1,12 @@
+from weather.utils.reader import read_file
+
+file_values = read_file()
+
+for file_value in file_values:
+    date = file_value[0]
+    max_temp = float(file_value[1])
+    min_temp = float(file_value[3])
+    diffrent_max_with_min = (max_temp - min_temp) /2
+
+    print(f"{date}, Maximum-Temp is {max_temp} AND Minimum-Temp is {min_temp} Diffrance, {diffrent_max_with_min}")
+
