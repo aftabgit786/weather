ba1721e1f58679ca97c8e1aa1507431d46fdb8a5
aftabgit786 <gb485585@gmail.com>
2023-03-20 16:12:43 +0500

    updated

--- tasks/t5.py
+++ tasks/t5.py
@@ -1,12 +1,10 @@
-from weather.utils.reader import read_file_for_events
+from weather.utils.reader import read_files
+file1_path = "/home/aftab/Desktop/Data/ISDP/weather/files/f1.csv"
+file2_path = "/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv"
 
-file_values = read_file_for_events()
-
-for file_value in file_values:
-
+file1_values, file2_values = read_files(file1_path, file2_path)
+for file_value in file2_values:
     date = file_value[1]
-
     events = file_value[-2]
-
     if events == "Rain" or events == "Snow" or events == "Rain-Snow":
         print(f"{events} of the date {date}")
--- utils/reader.py
+++ utils/reader.py
@@ -1,20 +1,18 @@
-def read_file():
-    file_values = []
-    with open("/home/aftab/Desktop/Data/ISDP/weather/files/f1.csv") as file_reader:
-        file_content = file_reader.read()
-        for row in file_content.split("\n")[1:-1]:
-            file_values.append(row.split(","))
-        file_reader.close()
+def read_files(file_path1, file_path2):
+    file_values1 = []
+    file_values2 = []
 
-        return file_values
+    with open(file_path1) as file_reader1:
+        file_content1 = file_reader1.read()
+        for row in file_content1.split("\n")[1:-1]:
+            file_values1.append(row.split(","))
+        file_reader1.close()
 
-def read_file_for_events():
-    file_values = []
-    with open("/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv") as file_reader:
-        file_content = file_reader.read()
-        for row in file_content.split("\n")[1:-1]:
-            file_values.append(row.split(","))
-        file_reader.close()
+    with open(file_path2) as file_reader2:
+        file_content2 = file_reader2.read()
+        for row in file_content2.split("\n")[1:-1]:
+            file_values2.append(row.split(","))
+        file_reader2.close()
 
-        return file_values
+    return file_values1, file_values2
 
