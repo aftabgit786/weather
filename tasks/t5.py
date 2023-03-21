dc4079d556f352f221bd2c7509765861558913d9
aftabgit786 <gb485585@gmail.com>
2023-03-21 13:10:02 +0500

    var

--- tasks/t5.py
+++ tasks/t5.py
@@ -1,8 +1,8 @@
 from weather.utils.reader import read_files
 
-file_content = read_files("/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv")
+file_values = read_files("/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv")
 
-for file_value in file_content:
+for file_value in file_values:
     date = file_value[1]
     events = file_value[-2]
     if events in ["Rain",  "Snow",  "Rain-Snow"]:
