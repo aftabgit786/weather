865ce6d869e8dd124dd0453d9f8b4af53d4531f8
aftabgit786 <gb485585@gmail.com>
2023-03-21 16:15:57 +0500

    t6

---
+++ tasks/t6.py
@@ -0,0 +1,11 @@
+from weather.utils.reader import read_files
+from datetime import datetime
+file_values = read_files("/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv")
+for file_value in file_values:
+    date = file_value[1]
+    events = file_value[-2]
+    if events == "Thunderstorm":
+        date_str = str(date)
+        date_to_names = datetime.strptime(date_str, "%Y-%m-%d")
+        day_name = date_to_names.strftime("%A")
+        print(f"{date_str} of wather is Thunderstorm and day is {day_name}")
