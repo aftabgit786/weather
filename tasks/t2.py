16281cff2a92b1dc5912491e3aecdb2f9bd61633
aftabgit786 <gb485585@gmail.com>
2023-03-21 16:02:02 +0500

    update

--- tasks/t2.py
+++ tasks/t2.py
@@ -1,12 +1,8 @@
-from weather.utils.reader import read_file
-
-file_values = read_file()
-
+from weather.utils.reader import read_files
+file_values = read_files("/home/aftab/Desktop/Data/ISDP/weather/files/f1.csv")
 for file_value in file_values:
     date = file_value[0]
     max_temp = float(file_value[1])
     min_temp = float(file_value[3])
-    average_of_max_with_min = (max_temp - min_temp) /2
-
+    average_of_max_with_min = (max_temp - min_temp) / 2
     print(f"{date}, Maximum-Temp is {max_temp} AND Minimum-Temp is {min_temp} of Average is: {average_of_max_with_min}")
-
