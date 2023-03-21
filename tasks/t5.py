9a2ac66893b9814ce7bbac8b9077229d902270b1
aftabgit786 <gb485585@gmail.com>
2023-03-21 13:02:03 +0500

    remove 1 function and 1 path

--- tasks/t5.py
+++ tasks/t5.py
@@ -1,10 +1,9 @@
 from weather.utils.reader import read_files
-file1_path = "/home/aftab/Desktop/Data/ISDP/weather/files/f1.csv"
-file2_path = "/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv"
 
-file1_values, file2_values = read_files(file1_path, file2_path)
-for file_value in file2_values:
+file_content = read_files("/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv")
+
+for file_value in file_content:
     date = file_value[1]
     events = file_value[-2]
-    if events in["Rain" or  "Snow" or  "Rain-Snow"]:
+    if events in ["Rain",  "Snow",  "Rain-Snow"]:
         print(f"{events} of the date {date}")
--- utils/reader.py
+++ utils/reader.py
@@ -1,18 +1,9 @@
-def read_files(file_path1, file_path2):
-    file_values1 = []
-    file_values2 = []
+def read_files(file_path):
+    file_values = []
+    with open(file_path) as file_reader:
+        file_content = file_reader.read()
+        for row in file_content.split("\n")[1:-1]:
+            file_values.append(row.split(","))
+        file_reader.close()
 
-    with open(file_path1) as file_reader1:
-        file_content1 = file_reader1.read()
-        for row in file_content1.split("\n")[1:-1]:
-            file_values1.append(row.split(","))
-        file_reader1.close()
-
-    with open(file_path2) as file_reader2:
-        file_content2 = file_reader2.read()
-        for row in file_content2.split("\n")[1:-1]:
-            file_values2.append(row.split(","))
-        file_reader2.close()
-
-    return file_values1, file_values2
-
+    return file_values
