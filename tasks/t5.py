4cead8734f47f6f83d44b1ad6f44eb96c8a667d3
aftabgit786 <gb485585@gmail.com>
2023-03-23 16:23:08 +0500

    modify

--- tasks/t5.py
+++ tasks/t5.py
@@ -1,5 +1,6 @@
 from weather.utils.reader import read_and_get_file_contents
 from weather.utils.constent import MappingForT5AndT6
+
 file_contents = read_and_get_file_contents("/home/aftab/Desktop/Data/ISDP/weather/files/f2.csv")
 
 for file_content in file_contents:
